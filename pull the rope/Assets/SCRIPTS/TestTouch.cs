9f07b32b52a6c5232cb5927141df21b00506d94b
DanielMoraC <43269179+DanielMoraC@users.noreply.github.com>
2018-09-30 11:58:34 +0200

    huha

--- pull the rope/Assets/SCRIPTS/TestTouch.cs
+++ pull the rope/Assets/SCRIPTS/TestTouch.cs
@@ -7,15 +7,51 @@ public class TestTouch : MonoBehaviour {
     Touch myTouch;
     Touch[] myTouches;
 
+    public int toquesI;
+    public int toquesD;
+    public int seconds;
+    int puntosI = 1;
+    int puntosD = 1;
+    bool posibleI = false;
+    bool posibleD = false;
+
+    public GameObject congelarI;
+    public GameObject dobleI;
+    public GameObject congelarD;
+    public GameObject dobleD;
+
+    public GameObject botonI;
+    public GameObject botonD;
+
 
     // Use this for initialization
     void Start () {
 
     }
-	
-	// Update is called once per frame
-    /*
-	void Update () {
+
+    // Update is called once per frame
+
+    void Update() {
+
+        if (toquesD == 10)
+        {
+            congelarD.SetActive(true);
+        } else if(toquesD == 20 && posibleD)
+        {
+            dobleD.SetActive(true);
+        }
+
+        if (toquesI == 10)
+        {
+            congelarI.SetActive(true);
+        }
+        else if (toquesI == 20 && posibleI)
+        {
+            dobleI.SetActive(true);
+        }
+
+
+        /*
         myTouch = Input.GetTouch(0);
 
         for(int i = 0; i < Input.touchCount; i++)
@@ -30,16 +66,82 @@ public class TestTouch : MonoBehaviour {
                 transform.Translate(+0.1f, 0, 0);
                 //Debug.Log("Adios");
             }
-        }
-	}*/
+        }*/
+    }
     public void MoverI()
     {
-        transform.Translate(-0.5f, 0, 0);
-        Debug.Log("Adios");
+        if (puntosI == 1)
+        {
+            transform.Translate(-0.5f, 0, 0);
+        }else if (puntosI == 2)
+        {
+            transform.Translate(-1f, 0, 0);
+        }
+        
+        //Debug.Log("Adios");
+        toquesI += puntosI;
     }
     public void MoverD()
     {
-        transform.Translate(+0.5f, 0, 0);
-        Debug.Log("Hola");
+        if (puntosD == 1)
+        {
+            transform.Translate(+0.5f, 0, 0);
+        }
+        else if (puntosD == 2)
+        {
+            transform.Translate(+1f, 0, 0);
+        }
+        //Debug.Log("Hola");
+        toquesD += puntosD;
+    }
+
+    public void FreezeI()
+    {
+        posibleI = true;
+        congelarI.SetActive(false);
+        StartCoroutine("CongI");        
+    }
+    public IEnumerator CongI()
+    {
+        botonD.SetActive(false);
+        yield return new WaitForSeconds(seconds);
+        botonD.SetActive(true);
+    }
+
+    public void FreezeD()
+    {
+        posibleD = true;
+        congelarD.SetActive(false);
+        StartCoroutine("CongD");
+    }
+    public IEnumerator CongD()
+    {
+        botonI.SetActive(false);
+        yield return new WaitForSeconds(seconds);
+        botonI.SetActive(true);
+    }
+
+    public void MultiplicadorI()
+    {
+        posibleI = false;
+        dobleI.SetActive(false);
+        StartCoroutine("DoubleI");
+    }
+    public IEnumerator DoubleI()
+    {
+        puntosI = 2;
+        yield return new WaitForSeconds(seconds);
+    }
+
+    public void MultiplicadorD()
+    {
+        posibleD = false;
+        dobleD.SetActive(false);
+        StartCoroutine("DoubleD");
+    }
+    public IEnumerator DoubleD()
+    {
+        puntosD = 2;
+        yield return new WaitForSeconds(seconds);
     }
 }
