e2b57cad43fe80be1949f66b4026ea74161d694b
MarcosBelenguer <MarcosBelenguer@users.noreply.github.com>
2018-09-30 16:08:46 +0200

    MenuPause

---
+++ pull the rope/Assets/SCRIPTS/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject Pause;
+
+    public void TogglePause()
+        {
+        Pause.SetActive(!Pause.activeSelf);
+        }
+
+    public void ToMenu ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+	
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
